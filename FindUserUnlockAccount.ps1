f48cda0daeabaa252fea07c5fb2776bb3d521331
Joel Musheno <joelmusheno@gmail.com>
2017-03-30 15:31:06 -0400

    inital commit of powershell scripts

---
+++ FindUserUnlockAccount.ps1
@@ -0,0 +1,13 @@
+﻿$user = Read-Host -Prompt 'Input the user name'
+$accountToUnlock = Search-ADAccount -LockedOut | where {$_.Name -like $user + "*" }
+
+if (-not $accountToUnlock){ 
+    $output =  "Could not find user name:{0} " -f $user
+    Write-Host $output
+    exit
+}
+
+Unlock-ADAccount -Identity $accountToUnlock -Verbose
+$output = "User: {0} full address:{1} unlocked" -f $accountToUnlock.Name, $accountToUnlock.DistinguishedName
+
+Write-Output $output
