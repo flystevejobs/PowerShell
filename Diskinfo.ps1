fecfc271b356612242c545b373b298dbc212f65b
flystevejobs <flystevejobs@me.com>
2017-03-30 15:48:02 -0400

    script to get disk info
    script to get disk info

---
+++ Diskinfo.ps1
@@ -0,0 +1,20 @@
+﻿<#
+.synopsis
+This is the short explaination
+.Description
+This is the long description
+.Parameter ComputerName
+This is for remote computers
+.Example
+Diskinfo -ComputerName remote
+This is for a remote computer
+#>
+function get-diskinfo{
+[cmdletbinding()]
+param(
+[parameter(mandatory=$true)]
+[string[]]$computername,
+$bogus
+)
+Get-WmiObject -ComputerName $computername win32_logicaldisk -Filter "deviceID='c:'"
+}
