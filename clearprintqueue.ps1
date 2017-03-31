a59a5562fd29d767862221a6fd740b598aea0557
flystevejobs <flystevejobs@me.com>
2017-03-31 11:53:56 -0400

    Clear Print Queue
    Clear the print queu of jobs stuck in print queue

---
+++ clearprintqueue.ps1
@@ -0,0 +1,20 @@
+﻿If ($PSCmdlet.ShouldProcess("Cancel print job(s)","Printer")) 
+{ 
+    If($PrinterInfo) 
+    { 
+        Try 
+        { 
+            Write-Verbose "Cancelling printer jobs." 
+            $PrinterInfo | Foreach{$_.CancelAllJobs()} 
+            Write-Host "Successfully cancel print job(s)." 
+        } 
+        Catch 
+        { 
+            Write-Host "Failed to cancel print jobs stuck in the print queue." 
+        } 
+    } 
+    Else 
+    { 
+        Write-Host "Cannot find any print jobs stuck in the print queue." 
+    } 
+} 
